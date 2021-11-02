0c782131a05da04774e97286c862b40cc4e9b230
Chloë Jane <chloe@jane.email>
2021-11-01 22:12:38 -0400

    expanded more on editor scripting, handling some events now and elaborated more on overall structure

--- js/editor.mjs
+++ js/editor.mjs
@@ -1,25 +1,87 @@
-class Entity {
-    z_index;
+/**
+ * Custom class hierarchy for events relevant to editor
+ */
+import {
+    MAP_BACKGROUND_STYLE,
+    MAP_NODE_ACTIVE_STYLE,
+    MAP_NODE_HOVER_STYLE,
+    MAP_NODE_STYLE
+} from "./style.mjs";
 
-    constructor() {
-        this.z_index = 0;
-    }
+class EditorEvent extends Event {
+    sourceEntity;
 
-    draw(ctx, x_px, y_px, px) {
-
-    }
-
-    contains_coordinate(x, y) {
-        return false;
-    }
-
-    handle_click() {
-        return false;   /* should return true if we handled this click (stop
-         looking for something else to handle it) */
+    constructor(type, sourceEntity) {
+        super("editor_" + type);
+        this.sourceEntity = sourceEntity;
     }
 }
 
-export class Rectangle extends Entity {
+/**
+ * Indicates no action should be taken in response to some UI interaction.
+ * (No-op)
+ */
+class NopEditorEvent extends EditorEvent {
+    constructor(sourceEntity) {
+        super("null", sourceEntity);
+    }
+}
+
+class Entity {
+    zIndex;
+    hovered = false;
+    active = false;
+
+    constructor() {
+        this.zIndex = 0;
+    }
+
+    draw(ctx, xToPx, yToPx, dToPx) {
+
+    }
+
+    coordinateCollides(x, y) {
+        return false;
+    }
+
+    handleMouseDown(dispatch) {
+        dispatch(new EditorEvent("mousedown", this));
+        return true;
+    }
+
+    handleMouseUp(dispatch) {
+        dispatch(new EditorEvent("mouseup", this));
+        return true;
+    }
+
+    handleMouseMove(dispatch) {
+        dispatch(new EditorEvent("mousemove", this));
+        return true;
+    }
+
+    handleWheel(dispatch, x, y, deltaX, deltaY) {
+        let e = new EditorEvent("wheel", this);
+        e.x = x;
+        e.y = y;
+        e.deltaX = deltaX;
+        e.deltaY = deltaY;
+        dispatch(e);
+        return true;
+    }
+
+    setHovered(state) {
+        this.hovered = state;
+    }
+
+    setActive(state) {
+        this.active = state;
+    }
+}
+
+/**
+ * Just a test class, not actually useful or part of end goal
+ */
+class Rectangle extends Entity {
     constructor(x, y, width, height, color) {
         super();
         this.x = x;
@@ -29,35 +91,102 @@ export class Rectangle extends Entity {
         this.color = color;
     }
 
-    draw(ctx, x_px, y_px, px) {
-        super.draw(ctx, x_px, y_px, px);
+    draw(ctx, xToPx, yToPx, dToPx) {
+        super.draw(ctx, xToPx, yToPx, dToPx);
 
         ctx.globalAlpha = 0.4
         ctx.fillStyle = this.color;
-        ctx.fillRect(x_px(this.x), y_px(this.y), px(this.width), px(this.height));
+        ctx.fillRect(xToPx(this.x), yToPx(this.y), dToPx(this.width), dToPx(this.height));
     }
 
 
-    contains_coordinate(x, y) {
+    coordinateCollides(x, y) {
         return x >= this.x
             && x <= this.x + this.width
             && y >= this.y
             && y <= this.y + this.height;
     }
 
-    handle_click() {
-        console.log("Clicked rectangle x=" + this.x + " y=" + this.y);
+    handleMouseDown(dispatch) {
+        dispatch(new EditorEvent("rect_mouse_down", this));
         return true;
     }
 }
 
-export class Viewport {
+class MapNode extends Entity {
+    x;
+    y;
+    style = MAP_NODE_STYLE;
+
+    constructor(x, y) {
+        super();
+        this.x = x;
+        this.y = y;
+    }
+
+
+    draw(ctx, xToPx, yToPx, dToPx) {
+        super.draw(ctx, xToPx, yToPx, dToPx);
+
+        ctx.globalAlpha = this.style.alpha;
+        ctx.fillStyle = this.style.fillColor;
+        ctx.strokeStyle = this.style.strokeColor;
+        ctx.lineWidth = this.style.strokeWidth;
+        ctx.beginPath();
+        ctx.arc(xToPx(this.x), yToPx(this.y), dToPx(this.style.radius), 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.stroke();
+    }
+
+    coordinateCollides(x, y) {
+        return Math.hypot(this.x - x, this.y - y) < this.style.radius;
+    }
+
+    handleMouseDown(dispatch) {
+        dispatch(new EditorEvent("map_node_mousedown", this));
+        return true;
+    }
+
+    handleMouseUp(dispatch) {
+        dispatch(new EditorEvent("map_node_mouseup", this));
+        return true;
+    }
+
+    handleMouseMove(dispatch) {
+        dispatch(new EditorEvent("map_node_mousemove", this));
+        return true;
+    }
+
+    setHovered(state) {
+        super.setHovered(state);
+
+        this.updateStyle();
+    }
+
+    setActive(state) {
+        super.setActive(state);
+
+        this.updateStyle();
+    }
+
+    updateStyle() {
+        if (this.active)
+            this.style = MAP_NODE_ACTIVE_STYLE;
+        else if (this.hovered)
+            this.style = MAP_NODE_HOVER_STYLE;
+        else
+            this.style = MAP_NODE_STYLE;
+    }
+}
+
+
+class Viewport extends EventTarget {
     // coordinate system x value aligning with left side of canvas
     x;
     // coordinate system y value aligning with top side of canvas
     y;
     // how many pixels are represented by one unit of our coordinate system
-    scale_factor;
+    scaleFactor;
 
     // we take over an entire canvas (for now, at least). Makes it
     // easier not needing to worry about overflow management.
@@ -65,83 +194,228 @@ export class Viewport {
     ctx;
 
     // scale coordinate x value to screen pixel x
-    x_px;
+    xToPx;
     // scale coordinate y value to screen pixel y
-    y_px;
+    yToPx;
     // scale coordinate distance to screen pixel distance
-    len_px;
-
+    dToPx;
     // scale screen pixel x to coordinate x
-    x_coord;
+    xFromPx;
     // scale screen pixel y to coordinate y
-    y_coord;
+    yFromPx;
     // scale screen pixel distance to coordinate distance
-    len_coord;
+    dFromPx;
 
     // list of instances we know about
     entities;
 
     constructor(canvas) {
+        super();
         this.canvas = canvas;
         let left = canvas.offsetLeft + canvas.clientLeft;
         let top = canvas.offsetTop + canvas.clientTop;
         let self = this;
-        this.canvas.addEventListener('click', function (event) {
-            let x = event.pageX - left;
-            let y = event.pageY - top;
-            self.handle_click(x, y);
+        this.canvas.addEventListener('mousedown', function (event) {
+            event.preventDefault();
+            let x = self.xFromPx(event.pageX - left);
+            let y = self.yFromPx(event.pageY - top);
+            self.handleMouseDown(x, y);
+        }, false);
+        this.canvas.addEventListener('mouseup', function (event) {
+            event.preventDefault();
+            let x = self.xFromPx(event.pageX - left);
+            let y = self.yFromPx(event.pageY - top);
+            self.handleMouseUp(x, y);
+        }, false);
+        this.canvas.addEventListener('mousemove', function (event) {
+            event.preventDefault();
+            let x = self.xFromPx(event.pageX - left);
+            let y = self.yFromPx(event.pageY - top);
+            self.handleMouseMove(x, y);
+        }, false);
+        this.canvas.addEventListener('wheel', function (event) {
+            event.preventDefault();
+            let x = self.xFromPx(event.pageX - left);
+            let y = self.yFromPx(event.pageY - top);
+            self.handleWheel(x, y, event.deltaX, event.deltaY);
         }, false);
         this.ctx = this.canvas.getContext('2d');
-        this.set_view(0, 0, 2)
+        this.setView(0, 0, 2)
         this.entities = []
     }
 
-    set_view(x, y, scale_factor) {
+    setView(x, y, scaleFactor) {
         this.x = x;
         this.y = y;
-        this.scale_factor = scale_factor;
+        this.scaleFactor = scaleFactor;
 
-        this.x_px = x => (x - this.x) * this.scale_factor;
-        this.y_px = y => (y - this.y) * this.scale_factor;
-        this.len_px = a => a * this.scale_factor;
+        this.xToPx = x => (x - this.x) * this.scaleFactor;
+        this.yToPx = y => (y - this.y) * this.scaleFactor;
+        this.dToPx = a => a * this.scaleFactor;
 
         // inverse of above functions
-        this.x_coord = x => this.x + (x / this.scale_factor);
-        this.y_coord = y => this.y + (y / this.scale_factor);
-        this.len_coord = a => a / this.scale_factor;
+        this.xFromPx = x => this.x + (x / this.scaleFactor);
+        this.yFromPx = y => this.y + (y / this.scaleFactor);
+        this.dFromPx = a => a / this.scaleFactor;
     }
 
-    add_entity(e) {
+    changeScaleCenteredOnPoint(x, y, newScaleFactor) {
+        // keep pixel x, y position constant (this.x and this.y will change,
+        // they represent the abstract position lining up with pixel
+        // positions x=0, y=0 respectively)
+
+        let px_x = this.xToPx(x)
+        let px_y = this.yToPx(y)
+
+        let new_x = x - px_x / newScaleFactor;
+        let new_y = y - px_y / newScaleFactor;
+
+        this.setView(new_x, new_y, newScaleFactor);
+    }
+
+    addEntity(e) {
         this.entities.push(e)
         // higher z-index = precedence for handling clicks; put them at
         // front of the list
         this.entities.sort((a, b) => {
-            return a.z_index < b.z_index;
+            return a.zIndex < b.zIndex;
         })
     }
 
     draw() {
+        this.ctx.fillStyle = MAP_BACKGROUND_STYLE.fillColor;
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
         for (let e of this.entities)
-            this.draw_entity(e);
+            this.drawEntity(e);
     }
 
-    draw_entity(e) {
+    drawEntity(e) {
         this.ctx.save()
 
-        e.draw(this.ctx, this.x_px, this.y_px, this.len_px);
+        e.draw(this.ctx, this.xToPx, this.yToPx, this.dToPx);
 
         this.ctx.restore()
     }
 
-    handle_click(x_px, y_px) {
-        let x = this.x_coord(x_px);
-        let y = this.y_coord(y_px);
-
+    handleMouseDown(x, y) {
         for (let e of this.entities) {
-            if (e.contains_coordinate(x, y) && e.handle_click())
-                break;
+            if (e.coordinateCollides(x, y) && e.handleMouseDown((e) => this.dispatchEvent(e)))
+                return;
+        }
+
+        this.dispatchEvent(new EditorEvent("bg_mousedown", null));
+    }
+
+    handleMouseUp(x, y) {
+        for (let e of this.entities) {
+            if (e.coordinateCollides(x, y) && e.handleMouseUp((e) => this.dispatchEvent(e)))
+                return;
+        }
+
+        this.dispatchEvent(new EditorEvent("bg_mouseup", null));
+    }
+
+    handleMouseMove(x, y) {
+        for (let e of this.entities) {
+            if (e.coordinateCollides(x, y) && e.handleMouseMove((e) => this.dispatchEvent(e)))
+                return;
+        }
+
+        this.dispatchEvent(new EditorEvent("bg_mousemove", null));
+    }
+
+    handleWheel(x, y, deltaX, deltaY) {
+        for (let e of this.entities) {
+            if (e.coordinateCollides(x, y)
+                && e.handleWheel((e) => this.dispatchEvent(e), x, y, deltaX, deltaY))
+                return;
+        }
+
+        let e = new EditorEvent("bg_wheel", this);
+        e.x = x;
+        e.y = y;
+        e.deltaX = deltaX;
+        e.deltaY = deltaY;
+        this.dispatchEvent(e);
+    }
+
+    addEditorEventListener(type, listener, options) {
+        this.addEventListener("editor_" + type, listener, options);
+    }
+
+    removeEditorEventListener(type, callback, options) {
+        this.removeEventListener("editor_" + type, callback, options);
+    }
+
+}
+
+export class Editor {
+    viewport;
+
+    hoveredEntity = null;
+    activeEntity = null;
+
+    constructor(canvas) {
+        this.viewport = new Viewport(canvas);
+        this.viewport.addEntity(new Rectangle(30, 81, 100, 50, 'rgb(100, 100,' +
+            ' 100'));
+        this.viewport.addEntity(new Rectangle(50, 100, 100, 150, 'rgb(100,' +
+            ' 200, 100'));
+        this.viewport.addEntity(new MapNode(200, 200));
+        this.viewport.draw()
+        let events = [
+            "mousedown",
+            "mouseup",
+            "mousemove",
+            "wheel",
+            "map_node_mousedown",
+            "map_node_mouseup",
+            "map_node_mousemove",
+            "map_node_wheel",
+            "bg_mousedown",
+            "bg_mouseup",
+            "bg_mousemove",
+            "bg_wheel",
+
+        ]
+        for (let e of events)
+            this.viewport.addEditorEventListener(e, (e) => this.handle_editor_event(e))
+
+        for (let e of events) {
+            if (e.endsWith("mousemove"))
+                this.viewport.addEditorEventListener(e, (e) => this.handle_editor_mousemove_event(e))
+
+        }
+        for (let e of events) {
+            if (e.endsWith("wheel"))
+                this.viewport.addEditorEventListener(e, (e) => this.handle_editor_wheel_event(e))
+
         }
     }
 
+    handle_editor_event(e) {
 
+    }
+
+    handle_editor_mousemove_event(e) {
+        if (e.sourceEntity !== this.hoveredEntity) {
+            this.hoveredEntity?.setHovered(false);
+            e.sourceEntity?.setHovered(true);
+
+            this.hoveredEntity = e.sourceEntity;
+
+            this.viewport.draw();
+        }
+    }
+
+    handle_editor_wheel_event(e) {
+        let newScale = this.viewport.scaleFactor + e.deltaY / 10.0;
+        if (newScale > 10)
+            newScale = 10;
+        if (newScale < 0.1)
+            newScale = 0.1;
+
+        this.viewport.changeScaleCenteredOnPoint(e.x, e.y, newScale);
+        this.viewport.draw();
+    }
 }
---
+++ js/style.mjs
@@ -0,0 +1,34 @@
+/**
+ * File to consolidate variables relating to styling javascript-produced
+ * graphics; any colors, stroke widths, etc. should be placed here if possible.
+ *
+ * I wonder if JS can pull this info from css?
+ */
+
+export const MAP_BACKGROUND_STYLE = {
+    fillColor: '#303040',
+}
+
+export const MAP_NODE_STYLE = {
+    radius: 25,
+    strokeColor: '#8080c0',
+    fillColor: '#505080',
+    alpha: 0.8,
+    strokeWidth: 2,
+}
+
+export const MAP_NODE_HOVER_STYLE = {
+    radius: 27,
+    strokeColor: '#b0b0f0',
+    fillColor: '#7070a0',
+    alpha: 0.8,
+    strokeWidth: 2,
+}
+
+export const MAP_NODE_ACTIVE_STYLE = {
+    radius: 25,
+    strokeColor: '#8080c0',
+    fillColor: '#505080',
+    alpha: 0.8,
+    strokeWidth: 2,
+}
